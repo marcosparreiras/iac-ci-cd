f8b5ed6a546754ed7b617be30051df05d712483a
Marcos Parreiras <marcosalp1998@gmail.com>
2024-06-17 13:19:03 -0300

    feat: add ecr-role

--- iam.tf
+++ iam.tf
@@ -7,3 +7,37 @@ resource "aws_iam_openid_connect_provider" "oidc_git" {
   }
 
 }
+
+
+resource "aws_iam_role" "ecr-role" {
+  name       = "ecr-role"
+  depends_on = [aws_iam_openid_connect_provider.oidc_git]
+
+  # Terraform's "jsonencode" function converts a
+  # Terraform expression result to valid JSON syntax.
+  assume_role_policy = jsonencode({
+    "Version" : "2012-10-17",
+    "Statement" : [
+      {
+        "Effect" : "Allow",
+        "Action" : "sts:AssumeRoleWithWebIdentity",
+        "Principal" : {
+          "Federated" : "arn:aws:iam::381492193067:oidc-provider/token.actions.githubusercontent.com"
+        },
+        "Condition" : {
+          "StringEquals" : {
+            "token.actions.githubusercontent.com:aud" : ["sts.amazonaws.com"],
+            "token.actions.githubusercontent.com:sub" : [
+              "repo:marcosparreiras/ci-cd:ref:refs/heads/main"
+            ]
+          }
+        }
+      }
+    ]
+    }
+  )
+
+  tags = {
+    IAC = "True"
+  }
+}
